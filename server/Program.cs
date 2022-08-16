de0697f57227e2d49f0dc7046ad88aabe3113bf6
Joshua LaBue <joshua@labue.me>
2022-08-15 22:33:20 -0400

    Add production build script

--- server/Program.cs
+++ server/Program.cs
@@ -5,9 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddCors(options => 
@@ -26,19 +23,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    // app.UseSwagger();
-    // app.UseSwaggerUI();
 }
-
-// app.UseHttpsRedirection();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.UseCors("AllowAll");
-
-// app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
